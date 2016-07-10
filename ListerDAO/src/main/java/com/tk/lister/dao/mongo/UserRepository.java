a988763f2a546d4a129fd9106b2bc2cfc10b0b00
Todd Krebs <krebstodd@gmail.com>
2016-07-10 16:45:41 -0400

    Update

--- ListerDAO/src/main/java/com/tk/lister/dao/mongo/UserRepository.java
+++ ListerDAO/src/main/java/com/tk/lister/dao/mongo/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  */
 public interface UserRepository extends MongoRepository<User, String>{
 
-    public User findByName(String name);
+    User findByName(String name);
 }
